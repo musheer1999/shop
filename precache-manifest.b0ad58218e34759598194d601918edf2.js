f152fd07fe7543283ba70bf5bdf3ca16533bff50
musheer1999 <musheerahmad.710@gmail.com>
2020-10-04 17:43:20 +0530

    Updates

---
+++ precache-manifest.b0ad58218e34759598194d601918edf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b3be313e78f96610350d5b6d70e7b3",
+    "url": "/github.io/shop/index.html"
+  },
+  {
+    "revision": "b3c3e5f69406dce88a8d",
+    "url": "/github.io/shop/static/css/2.b6ac9e22.chunk.css"
+  },
+  {
+    "revision": "7e2a65979114fce6a47a",
+    "url": "/github.io/shop/static/css/main.ea1027fc.chunk.css"
+  },
+  {
+    "revision": "b3c3e5f69406dce88a8d",
+    "url": "/github.io/shop/static/js/2.b5e87051.chunk.js"
+  },
+  {
+    "revision": "7e2a65979114fce6a47a",
+    "url": "/github.io/shop/static/js/main.2d2a73ca.chunk.js"
+  },
+  {
+    "revision": "3193859693137bd8d14f",
+    "url": "/github.io/shop/static/js/runtime~main.b7b7b72d.js"
+  }
+]);
